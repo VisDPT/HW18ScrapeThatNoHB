0be6f924b31b722c84f7f91cfe8bc15830a3e371
Vis Vasanth <viscodesdpt@gmail.com>
2016-12-12 19:09:59 -0600

    pls work

--- public/app.js
+++ public/app.js
@@ -23,7 +23,7 @@ $(document).on("click", "p", function() {
         // With that done, add the note information to the page
         .done(function(data) {
             console.log(data);
-            // The title of the article
+           
             $("#notes").append("<h2>" + data.title + "</h2>");
             // An input to enter a new title
             $("#notes").append("<input id='titleinput' name='title' >");
